79c49d74cdb1e865d75b2f4a7bea1f9e2dc80f12
Mateus Medeiros Garcia <mateus.medeiros@ufpr.br>
2023-02-08 19:35:39 -0300

    Initial commit

---
+++ src/main/java/com/bantads/orchestratorSaga/OrchestratorSagaApplication.java
@@ -0,0 +1,13 @@
+package com.bantads.orchestratorSaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratorSagaApplication.class, args);
+	}
+
+}
