01175d81f39d7bac9a1ce90e17b2b45834888d9c
Mateus Medeiros Garcia <mateus.medeiros@ufpr.br>
2023-02-09 12:55:55 -0300

    Added customer producer logic

---
+++ src/main/java/com/bantads/orchestratorSaga/producer/CustomerProducer.java
@@ -0,0 +1,39 @@
+package com.bantads.orchestratorSaga.producer;
+
+import com.bantads.orchestratorSaga.config.CustomerConfiguration;
+import com.bantads.orchestratorSaga.model.CustomerModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RestController()
+@RequestMapping("/custumer")
+public class CustomerProducer {
+
+
+    @Autowired private RabbitTemplate rabbitTemplate;
+    @Autowired private DirectExchange exchange;
+
+    @PostMapping
+    public void create(@RequestBody CustomerModel customerModel) {
+        rabbitTemplate.convertAndSend(exchange.getName(), CustomerConfiguration.createCustomerRouting, customerModel);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id, @RequestBody CustomerModel customerModel) {
+        customerModel.setId(id);
+        rabbitTemplate.convertAndSend(exchange.getName(), CustomerConfiguration.updateCustomerRouting, customerModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        rabbitTemplate.convertAndSend(exchange.getName(), CustomerConfiguration.deleteCustomerRouting, id);
+    }
+}
